f316335b46d7f01475303a26d9ab22a2eb057e39
Chandan Sharma <chandansharma79928.fea@gmail.com>
2024-04-05 18:58:54 +0530

    add some changes

--- App.jsx
+++ App.jsx
@@ -11,8 +11,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {/* <Stack.Screen name="Signup" component={Signup} /> */}
-        {/* <Stack.Screen name="Login" component={Login} /> */}
+        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Homepage" component={HomePage} />
         <Stack.Screen name="Products" component={PorductDetails} />
       </Stack.Navigator>
